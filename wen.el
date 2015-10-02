229a2da6856a97e6e4412d226efec9548a5519ff
Wensheng Chen <wchen@money-media.com>
2015-10-02 15:00:14 -0400

    make comments more readable, disable large ile warning, this is needed because TAGS file is too big, added a mode for js, change to dos mode on save, this is so that files are saved in LF rather than CF

--- wen.el
+++ wen.el
@@ -1,4 +1,11 @@
-;; Define flycheck syntax checker for PHP and Web mode
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Set custom emacs variables                          ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(setq large-file-warning-threshold nil) ; Disables large file warning, mainly for TAGS file
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Define flycheck syntax checker for PHP and Web mode ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (require 'flycheck)
 (flycheck-define-checker my-php
   "A PHP syntax checker using the PHP command line interpreter.
@@ -11,10 +18,16 @@ See URL `http://php.net/manual/en/features.commandline.php'."
           (message) " in " (file-name) " on line " line line-end))
   :modes (php-mode php+-mode web-mode))
 
-;; Add the newly defined checker to list of flycheckers
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Add the newly defined checker to list of flycheckers ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (add-to-list 'flycheck-checkers 'my-php)
 
-;; Web mode for PHP
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;     Define custom mode for PHP                       ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun my-setup-php ()
   ;; enable web mode
   (web-mode)
@@ -45,7 +58,7 @@ See URL `http://php.net/manual/en/features.commandline.php'."
   (auto-complete-mode t)
 
   ;; Load TAGS
-  (visit-tags-table "/opt/git/ReleaseApp/vendor/mm/TAGS")
+  (visit-tags-table "/opt/git/WebApps/PHP_TAGS")
 
   ; expanders: typing d/s/ will expand to <div><span>|</span></div> (see web-mode-expanders).
   ;(setq web-mode-enable-auto-expanding t)
@@ -55,7 +68,6 @@ See URL `http://php.net/manual/en/features.commandline.php'."
 
   ;; Set comment format
   (add-to-list 'web-mode-comment-formats '("php" . "//"))
-
 
   ;; enable flycheck-mode using my-php
   (require 'flycheck)
@@ -67,18 +79,78 @@ See URL `http://php.net/manual/en/features.commandline.php'."
   ;;       '(("php" . (("vd" . ("var_dump(|);\ndie();"))))
   ;;         ))
 
-  ;; PHP Function snippet
+  ;; enable yasnippet
   (require 'yasnippet)
-  (yas-global-mode 1)
+  (yas-global-mode t)
+  ;; (yas-reload-all t)
   ;; Create symlink to elpa yasnippet snippets directory to use them without needing to add another directory and reloading the snippets (SLOWER)
   ;; (add-to-list 'yas-snippet-dirs "/home/wen/.emacs.d/snippets/text-mode/")
   )
 
-;; Trigger my-setup-php when opening files matching the given pattern
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;     Define custom mode for JavaScript                ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun my-setup-js ()
+  (require 'auto-complete)
+  (auto-complete-mode t)
+
+  (add-hook 'js2-mode-hook (lambda () (tern-mode t)))
+  (eval-after-load 'tern
+    '(progn
+       (require 'tern-auto-complete)
+       (tern-ac-setup)))
+
+  ;; enable js2 mode, javascript-mode is deprecated
+  (js2-mode)
+
+  ;; js2-mode provides 4 level of syntax highlighting. They are
+  ;; * 0 or a negative value means none.
+  ;; * 1 adds basic syntax highlighting.
+  ;; * 2 adds highlighting of some Ecma built-in properties.
+  ;; * 3 adds highlighting of many Ecma built-in functions.
+  (setq js2-highlight-level 3)
+
+  ;; enable auto-complete-mode
+  ;; (require 'auto-complete)
+  ;; (auto-complete-mode t)
+  ;; (require 'tern)
+  ;; (tern-mode t)
+  ;; (require 'tern-auto-complete)
+  ;; (tern-ac-setup t)
+
+  ;; Load TAGS
+  ;;(visit-tags-table "/opt/git/WebApps/JS_TAGS")
+
+  ;; enable yasnippet
+  (require 'yasnippet)
+  (yas-global-mode t)
+  ;; (yas-reload-all t)
+  ;; Create symlink to elpa yasnippet snippets directory to use them without needing to add another directory and reloading the snippets (SLOWER)
+  ;; (add-to-list 'yas-snippet-dirs "/home/wen/.emacs.d/snippets/text-mode/")
+  )
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Trigger appropriate modes when opening files                       ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (add-to-list 'auto-mode-alist '("\\.php$" . my-setup-php))
 (add-to-list 'auto-mode-alist '("\\.phtml$" . my-setup-php))
+(add-to-list 'auto-mode-alist '("\\.js$" . my-setup-js))
+(add-to-list 'auto-mode-alist '("\\.json$" . my-setup-js))
 
-;; Remove trailing whitespace on save
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Remove trailing whitespace and save as unix line ending on save ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun before-save ()
+  (setq-default buffer-file-coding-system 'utf-8-unix)
+  (setq-default default-buffer-file-coding-system 'utf-8-unix)
+  (set-default-coding-systems 'utf-8-unix)
+  (prefer-coding-system 'utf-8-unix)
+
+  (delete-trailing-whitespace t)
+)
+
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 ;; if indent-tabs-mode is off, untabify before saving
 (add-hook 'write-file-hooks
@@ -86,7 +158,10 @@ See URL `http://php.net/manual/en/features.commandline.php'."
                          (untabify (point-min) (point-max)))
             nil ))
 
-;; PHPUnit
+
+;;;;;;;;;;;;;;;;;;;;;
+;;     PHPUnit     ;;
+;;;;;;;;;;;;;;;;;;;;;
 (require 'phpunit)
 (defun phpunit-get-program (args)
   "Return the command to launch unit test.
